7332e4cc3c03eb1b445272ecc16b3bb51bff53d4
Bhargavi Poosarla <bhargavi.poosarla@ad1.searceinc.it>
2022-09-25 14:16:46 +0530

    initial: Initial login and registration page codes

---
+++ lib/screens/signup.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+
+import 'LinkedIn_Signup.dart';
+import 'loginscreenpage.dart';
+//import 'loginscreenpage.dart';
+
+
+class MyRegister extends StatefulWidget {
+  const MyRegister({Key key}) : super(key: key);
+
+  @override
+  _MyRegisterState createState() => _MyRegisterState();
+}
+
+class _MyRegisterState extends State<MyRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        // appBar: AppBar(
+        //   centerTitle: true,
+        //   title: Text(
+        //     '',
+        //     style: TextStyle(color: Colors.white, fontSize: 33),
+        //   ),
+        //   backgroundColor: Colors.transparent,
+        //   elevation: 0,
+        // ),
+        body: Stack(
+          children: [
+
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.14),
+
+
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 35, right: 35),
+                      child: Column(
+                        children: [
+
+                          Text("Create Your Account",
+                            style: TextStyle(
+                                color: Colors.white,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+
+                          TextField(
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                hintText: "Name",
+                                hintStyle: TextStyle(color: Colors.white),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          TextField(
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                hintText: "Email",
+                                hintStyle: TextStyle(color: Colors.white),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          TextField(
+                            style: TextStyle(color: Colors.white),
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                hintText: "Password",
+                                hintStyle: TextStyle(color: Colors.white),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+
+                          TextField(
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                hintText: "LinkedIn ID",
+                                hintStyle: TextStyle(color: Colors.white),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                )),
+                          ),
+
+                          SizedBox(
+                            height: 40,
+                          ),
+
+
+                          TextButton(
+                            onPressed: () {
+                              //Navigator.pushNamed(context, 'register');
+
+                              Navigator.push(
+                                context,
+
+                                MaterialPageRoute(builder: (context) => const MyApp()),
+                              );
+
+
+                            },
+                            child: Text(
+                              'Sign Up With LinkedIn',
+                              style: TextStyle(
+                                  decoration: TextDecoration.underline,
+
+                                  color: Colors.white,
+                                  fontSize: 27,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            style: ButtonStyle(),
+                          ),
+
+
+
+                          SizedBox(
+                            height: 40,
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              //Navigator.pushNamed(context, 'register');
+
+                              Navigator.push(
+                                context,
+
+                                MaterialPageRoute(builder: (context) => const Login()),
+                              );
+
+
+                            },
+                            child: Text(
+                              'Or Login',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  color: Colors.white,
+                                  fontSize: 18),
+                            ),
+                            style: ButtonStyle(),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
